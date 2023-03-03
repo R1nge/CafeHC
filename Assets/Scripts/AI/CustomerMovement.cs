df46078c9239bdcb814918911096819cb61eafdf
R1nge <r1ngegamedev@gmail.com>
2023-03-03 11:52:31 +0300

    Backup

---
+++ Assets/Scripts/AI/CustomerMovement.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+namespace AI
+{
+    public class CustomerMovement : MonoBehaviour
+    {
+        private NavMeshAgent _navMeshAgent;
+
+        private void Awake()
+        {
+            _navMeshAgent = GetComponent<NavMeshAgent>();
+        }
+
+        public void SetDestination(Vector3 position)
+        {
+            _navMeshAgent.SetDestination(position);
+        }
+    }
+}
