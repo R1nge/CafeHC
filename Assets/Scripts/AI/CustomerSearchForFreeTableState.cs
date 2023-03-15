418e0f8caa2201cfdc80ee987e2b15f64aa677a9
R1nge <r1ngegamedev@gmail.com>
2023-03-15 18:39:38 +0300

    [Refactor] Table, search for table state

--- Assets/Scripts/AI/CustomerSearchForFreeTableState.cs
+++ Assets/Scripts/AI/CustomerSearchForFreeTableState.cs
@@ -8,35 +8,48 @@ namespace AI
         private readonly TableManager _tableManager;
         private readonly CustomerMovement _customerMovement;
         private readonly Waypoints _waypoints;
+        private bool _hasFoundTable;
 
-        public CustomerSearchForFreeTableState(TableManager tableManager, CustomerMovement customerMovement, Waypoints waypoints)
+        public CustomerSearchForFreeTableState(TableManager tableManager, CustomerMovement customerMovement,
+            Waypoints waypoints)
         {
             _tableManager = tableManager;
             _customerMovement = customerMovement;
             _waypoints = waypoints;
         }
 
-        //TODO: redo
         public void Enter()
         {
-            if (_tableManager.GetFreeTable() == null) return;
-            if (!_tableManager.GetFreeTable().HasFreeSeat()) return;
-            _waypoints.RemoveCustomer();
-            _customerMovement.MoveTo(_tableManager.GetFreeTable().GetFreeSeat().GetPosition());
-            Debug.Log("FOUND FREE TABLE");
+            if (TryFindTable())
+            {
+                _hasFoundTable = true;
+            }
         }
 
         public void Exit()
         {
-            
         }
 
         public void Update()
         {
+            if (_hasFoundTable) return;
+            if (TryFindTable())
+            {
+                _hasFoundTable = true;
+            }
         }
 
         public void OnTriggerEnter(Collider other)
         {
+        }
+
+        private bool TryFindTable()
+        {
+            if (_tableManager.GetFreeTable() == null) return false;
+            if (!_tableManager.GetFreeTable().HasFreeSeat()) return false;
+            _waypoints.RemoveCustomer();
+            _customerMovement.MoveTo(_tableManager.GetFreeTable().GetFreeSeat().GetPosition());
+            return true;
         }
     }
 }
--- Assets/Scripts/Tables/Table.cs
+++ Assets/Scripts/Tables/Table.cs
@@ -1,13 +1,25 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
 using System.Diagnostics;
+using AI;
 using UnityEngine;
+using Zenject;
 
 namespace Tables
 {
     public class Table : MonoBehaviour
     {
+        [SerializeField] private float eatInterval;
+        [SerializeField] private GameObject garbage;
+        [SerializeField] private MoneyArea moneyArea;
         [SerializeField] private List<Seat> seats;
-        
+        private readonly GarbageItem _garbageItem = new();
+        private Inventory _inventory;
+        private CoffeeFactory _coffeeFactory;
+        private int _currentCount;
+        private int _eatenCount;
+        private YieldInstruction _coroutine;
+
         [Conditional("UNITY_EDITOR")]
         public void FindSeats()
         {
@@ -21,20 +33,11 @@ namespace Tables
             }
         }
 
-        public void FreeUp()
-        {
-            for (int i = 0; i < seats.Count; i++)
-            {
-                seats[i].GetCustomer().SetCustomerGoHome();
-                seats[i].SetCustomer(null);
-            }
-        }
-
         public bool HasFreeSeat()
         {
             for (int i = 0; i < seats.Count; i++)
             {
-                if (seats[i].GetCustomer())
+                if (seats[i].GetCustomer() || garbage.activeInHierarchy)
                 {
                     continue;
                 }
@@ -49,15 +52,140 @@ namespace Tables
         {
             for (int i = 0; i < seats.Count; i++)
             {
-                if (seats[i].GetCustomer())
+                if (seats[i].GetCustomer() || garbage.activeInHierarchy)
                 {
                     continue;
                 }
-                
+
                 return seats[i];
             }
 
             return null;
+        }
+
+        [Inject]
+        private void Construct(CoffeeFactory coffeeFactory) => _coffeeFactory = coffeeFactory;
+
+        private void Awake()
+        {
+            _inventory = GetComponent<Inventory>();
+            _inventory.OnAllItemsRemovedEvent += OnAllItemsRemoved;
+            _inventory.OnItemAddedEvent += OnItemAdded;
+            _inventory.OnItemRemovedEvent += OnItemRemoved;
+        }
+
+        private void OnItemAdded(InventoryItem item)
+        {
+            var pos = new Vector3(
+                transform.position.x,
+                transform.position.y + 1f + 0.5f * _currentCount,
+                transform.position.z
+            );
+
+            _coffeeFactory.GetFromPool(pos, Quaternion.identity, transform);
+            _currentCount = _inventory.GetCount();
+
+            if (HasFreeSeat()) return;
+            _coroutine ??= StartCoroutine(Eat_c());
+        }
+
+        private void OnAllItemsRemoved(InventoryItem item)
+        {
+            for (int i = transform.childCount - 1; i >= 0; i--)
+            {
+                _coffeeFactory.ReturnToPool(transform.GetChild(i).gameObject);
+            }
+
+            _currentCount = 0;
+        }
+
+        private void OnItemRemoved(InventoryItem item)
+        {
+            _coffeeFactory.ReturnToPool(transform.GetChild(transform.childCount - 1).gameObject);
+            _currentCount--;
+            _eatenCount++;
+            //moneyArea.AddMoney(Random.Range(5, 10));
+        }
+
+        private IEnumerator Eat_c()
+        {
+            while (_currentCount >= 0)
+            {
+                if (_currentCount == 0)
+                {
+                    EnableTrash();
+                    FreeUpTable();
+                    StopAllCoroutines();
+                    _coroutine = null;
+                    yield break;
+                }
+
+                yield return new WaitForSeconds(eatInterval);
+
+                _inventory.RemoveItem(_inventory.GetItem());
+            }
+        }
+
+        private void EnableTrash()
+        {
+            garbage.SetActive(true);
+        }
+
+        private void FreeUp()
+        {
+            for (int i = 0; i < seats.Count; i++)
+            {
+                seats[i].GetCustomer().SetCustomerGoHome();
+                seats[i].SetCustomer(null);
+            }
+        }
+
+        private void Clean(PlayerInventory inventory)
+        {
+            if(!inventory.TryAddItem(_garbageItem)) return;
+            for (int i = 0; i < _eatenCount; i++)
+            {
+                inventory.TryAddItem(_garbageItem);
+            }
+
+            _eatenCount = 0;
+            garbage.SetActive(false);
+        }
+
+        private void FreeUpTable()
+        {
+            FreeUp();
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (other.TryGetComponent(out CustomerInventory customerInventory))
+            {
+                if (customerInventory.TryGetComponent(out CustomerStateManager customerStateManager))
+                {
+                    if (!HasFreeSeat()) return;
+                    GetFreeSeat().SetCustomer(customerStateManager);
+                    var count = customerInventory.GetCount();
+                    for (int i = count - 1; i >= 0; i--)
+                    {
+                        customerInventory.TryTransferTo(_inventory);
+                    }
+                }
+            }
+            else if (other.TryGetComponent(out PlayerInventory playerInventory))
+            {
+                if (garbage.activeInHierarchy)
+                {
+                    Clean(playerInventory);
+                }
+            }
+        }
+
+        private void OnDestroy()
+        {
+            _inventory.OnAllItemsRemovedEvent -= OnAllItemsRemoved;
+            _inventory.OnItemAddedEvent -= OnItemAdded;
+            _inventory.OnItemRemovedEvent -= OnItemRemoved;
         }
     }
 }
